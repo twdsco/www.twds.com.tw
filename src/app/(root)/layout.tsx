b5f8021c9ec395e9bb730cd31cda5084c39cd362
Jasper Yu <007seadog@gmail.com>
2025-07-06 10:35:57 +0800

    Added (root) for static build

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { getTranslations } from "next-intl/server";
+
+const locale = 'zh-tw';
+const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+export const metadata: Metadata = {
+  icons: [{ rel: "icon", url: "/logo/twds_circle.svg" }],
+  title: t('title'),
+  description: t('description'),
+  keywords: "networking, security, open source, sustainability, technology, startups, future, Taiwan, sustainability, education, cybersecurity",
+  authors: [{ name: "Taiwan Digital Streaming Co." }],
+  openGraph: {
+    title: t('title'),
+    description: t('description'),
+    type: "website",
+    locale: locale === 'zh-tw' ? 'zh_TW' : 'en_US',
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: t('title'),
+    description: t('description'),
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="zh-TW">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+} 
